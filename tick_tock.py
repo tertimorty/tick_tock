b5a5d329346fd94eb58976870f88a5d74c8d5637
tertimorty <kaspar.blumberg@gmail.com>
2021-03-13 21:42:40 +0200

    final version

--- tick_tock.py
+++ tick_tock.py
@@ -1,8 +1,3 @@
-# making a tick tack game
-# 1.) create board
-# 2.)update board
-# 3.) make everything in seperate functions and game engine just /
-# calls functions in order while game is valid
 from colorama import *
 import sys
 from termcolor import colored, cprint
@@ -12,21 +7,19 @@ empty_space = ' '
 
 def tick_tock_game():
     '''this is simple tick tock game played in terminal '''
-    # Generate board and tell that bame has started
+    # Generate board and tell that game has started
     text = colored('Welcome to TICK TOCK game!!', 'cyan', attrs=['blink'])
     print(text)
-    time.sleep(2)
+    time.sleep(1)
     print(Fore.YELLOW + f'When you are ready insert position in which you \
 whant your character')
-    time.sleep(3)
+    time.sleep(1)
     print(f'The one who makes three in a row wins!' + Style.RESET_ALL)
-    time.sleep(2)
-
-    # This is place where intro is finished and game begins, the x
-    # initialized below is used to diferentiate between players to
-    # know which ones turn it is
+    time.sleep(1)
     wrong_choice_text = f'You can not place your tile in that space, choose \
 another place'
+    invalid_a_txt = f'You entered invalid adress'
+    w_txt = f'You Won player '
     x = 1
     vin_condition = 0
     space_values = {'a1': empty_space, 'a2': empty_space, 'a3': empty_space,
@@ -39,22 +32,31 @@ another place'
             input_text = 'X'
             x = 0
             while True:
-                place = input(f'Where do you whant to place {input_text}? ')
+                place_x = input(f'Where do you whant to place {input_text}? ')
                 try:
-                    if space_values[place] == empty_space:
-                        space_values[place] = 'X'
+                    if space_values[place_x] == empty_space:
+                        space_values[place_x] = 'X'
                         print_chooser(space_values)
                         break
-                    elif space_values[place] != empty_space:
+                    elif space_values[place_x] != empty_space:
                         print(Fore.RED + wrong_choice_text + Style.RESET_ALL)
                 except KeyError:
-                    print(Fore.RED + f'You entered invalid adress' +
-                            Style.RESET_ALL)
+                    print(Fore.RED + invalid_a_txt + Style.RESET_ALL)
 
-            vin_condition = check_for_victory(space_values)
-            if vin_condition == 1:
-                print(Fore.YELLOW + f'You won player {input_text}' + Style.RESET_ALL)
-                break
+            if check_for_victory(space_values) == 1:
+                print(Fore.YELLOW + w_txt + f'{input_text}' + Style.RESET_ALL)
+                play_again1 = play_again(space_values)
+                if play_again1 != 'y':
+                    break
+                else:
+                    print_chooser(space_values)
+            if check_for_draw(space_values) != 1:
+                print(Fore.YELLOW + f'This is a draw' + Style.RESET_ALL)
+                play_again1 = play_again(space_values)
+                if play_again1 != 'y':
+                    break
+                else:
+                    print_chooser(space_values)
 
         elif x == 0:
             input_text = 'O'
@@ -69,13 +71,22 @@ another place'
                     elif space_values[palce_o] != empty_space:
                         print(Fore.RED + wrong_choice_text + Style.RESET_ALL)
                 except KeyError:
-                    print(Fore.RED + f'You entered invalid adress' +
-                            Style.RESET_ALL)
+                    print(Fore.RED + invalid_a_txt + Style.RESET_ALL)
 
-            vin_condition = check_for_victory(space_values)
-            if vin_condition == 1:
-                print(Fore.YELLOW + f'You won player {input_text}' + Style.RESET_ALL)
-                break
+            if check_for_victory(space_values) == 1:
+                print(Fore.YELLOW + w_txt + f'{input_text}' + Style.RESET_ALL)
+                play_again1 = play_again(space_values)
+                if play_again1 != 'y':
+                    break
+                else:
+                    print_chooser(space_values)
+            if check_for_draw(space_values) != 1:
+                print(Fore.YELLOW + f'This is a draw' + Style.RESET_ALL)
+                play_again1 = play_again(space_values)
+                if play_again1 != 'y':
+                    break
+                else:
+                    print_chooser(space_values)
 
 
 def print_red(input_str):
@@ -85,44 +96,9 @@ def print_red(input_str):
 def print_blue(input_str):
     print(Fore.BLUE + f'{input_str}', end="" + Style.RESET_ALL)
 
+
 def print_with_space(input_str):
     print(f'{input_str}', end="")
-
-
-space_values = {'a1': empty_space, 'a2': empty_space, 'a3': empty_space,
-                    'b1': empty_space, 'b2': empty_space, 'b3': empty_space,
-                    'c1': empty_space, 'c2': empty_space, 'c3': empty_space}
-
-
-def print_board(space_values):
-    '''based on input and what not chooses which print to use for each of lines'''
-
-    print(f"    a   b   c")
-    print(f"  -------------")
-    print_with_space(f"1 | ")
-    print_red(f"{space_values['a1']}")
-    print_with_space(f" | ")
-    print_red(f"{space_values['b1']}")
-    print_with_space(f" | ")
-    print_red(f"{space_values['c1']}")
-    print(f" | ")
-    print(f"  -------------")
-    print_with_space(f"2 | ")
-    print_blue(f"{space_values['a2']}")
-    print_with_space(f" | ")
-    print_blue(f"{space_values['b2']}")
-    print_with_space(f" | ")
-    print_blue(f"{space_values['c2']}")
-    print(f" | ")
-    print(f"  -------------")
-    print_with_space(f"3 | ")
-    print_red(f"{space_values['a3']}")
-    print_with_space(f" | ")
-    print_red(f"{space_values['b3']}")
-    print_with_space(f" | ")
-    print_red(f"{space_values['c3']}")
-    print(f" | ")
-    print(f"  -------------")
 
 
 def check_for_victory(space_values):
@@ -153,7 +129,12 @@ def check_for_victory(space_values):
         return 1
 
 
-# tick_tock_game()
+def check_for_draw(space_values):
+    '''This function checks if there is draw'''
+    for key in space_values:
+        if space_values[key] == ' ':
+            return 1
+
 
 def print_chooser(space_values):
     printable_list = ["    a   b   c", "  -------------"]
@@ -195,6 +176,14 @@ def print_chooser(space_values):
         print_with_space(f" | ")
     print(f'')
     print(f"  -------------")
+
+
+def play_again(space_values):
+    play_again = input('Do you want to play again y/n?')
+    if play_again == 'y':
+        for key in space_values:
+            space_values[key] = empty_space
+        return 'y'
 
 
 tick_tock_game()
